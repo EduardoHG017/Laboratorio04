2aead5d25d7c3b6485c7e513002565cbfb41f87a
Eduardo <edhernandezgi@correo.url.edu.gt>
2023-04-28 12:10:43 -0600

    Ejercicio 05

--- Laboratorio04/Ejercicio05.cs
+++ Laboratorio04/Ejercicio05.cs
@@ -10,28 +10,7 @@ namespace Laboratorio04
     {
         public int[] CalcularBilletes(int cantidad, int[,] billetes)
         {
-            int[] resultado = new int[billetes.GetLength(0)];
-
-            for (int i = 0; i < resultado.Length; i++)
-            {
-                int denominacion = billetes[i, 0];
-                int limiteDenominacion = billetes[i, 1];
-
-                int posiblesBilletes = cantidad / denominacion;
-
-                if (limiteDenominacion < 0 || posiblesBilletes <= limiteDenominacion)
-                {
-                    resultado[i] = posiblesBilletes;
-                }
-                else
-                {
-                    resultado[i] = limiteDenominacion;
-                }
-
-                cantidad = cantidad - (resultado[i] * denominacion);
-            }
-
-            return resultado;
+            return new int[] { };
         }
     }
 }
